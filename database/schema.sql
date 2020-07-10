7b65de7b8ce5a0d3cd7471d230b4dc516ea7f6e7
Nathan Sou <nathanksou@gmail.com>
2020-07-10 15:41:57 -0700

    Implement schema cql and sql and datageneration script

--- database/schema.sql
+++ database/schema.sql
@@ -1,146 +1,41 @@
-/*  Execute this file from the command line by typing:
+/*  Execute this file from the root directory in the command line by typing:
  *    mysql -u root -p < database/schema.sql
  *  to create the database and the tables. */
 
-CREATE DATABASE photos;
+CREATE DATABASE daintree;
 
-USE photos;
+USE daintree;
 
 CREATE TABLE hotels (
   hotelId         INT AUTO_INCREMENT PRIMARY KEY,
-  hotelName       VARCHAR(255),
-  hotelCity       VARCHAR(255),
-  hotelPrice      INT,
+  hotelName       VARCHAR(255) NOT NULL,
+  hotelCity       VARCHAR(255) NOT NULL,
+  hotelPrice      INT NOT NULL,
   numReviews      INT
 );
 
 CREATE TABLE users (
   userId          INT AUTO_INCREMENT PRIMARY KEY,
-  user            VARCHAR(255),
-  userAvatarUrl   VARCHAR(255)
+  user            VARCHAR(255) NOT NULL,
+  userAvatarUrl   VARCHAR(255),
+  locationCity    VARCHAR(255),
+  locationState   VARCHAR(255),
+  contributions   INT
 );
 
-CREATE TABLE rooms (
-  roomId          INT AUTO_INCREMENT PRIMARY KEY,
-  imageUrl        VARCHAR(255),
-  caption         VARCHAR(255),
-  category        VARCHAR(255),
+CREATE TABLE photos (
+  photoId         INT AUTO_INCREMENT PRIMARY KEY,
+  imageUrl        VARCHAR(255) NOT NULL,
+  category        VARCHAR(255) NOT NULL,
   datePosted      VARCHAR(255),
-  helpfulVotes    INT,
-  location        VARCHAR(255),
-  contributions   INT,
-  hotelId         INT,
-  userId          INT
-);
-
-CREATE TABLE dinings (
-  diningId        INT AUTO_INCREMENT PRIMARY KEY,
-  imageUrl        VARCHAR(255),
   caption         VARCHAR(255),
-  category        VARCHAR(255),
-  datePosted      VARCHAR(255),
   helpfulVotes    INT,
-  location        VARCHAR(255),
-  contributions   INT,
-  hotelId         INT,
-  userId          INT
+  hotelId         INT NOT NULL,
+  userId          INT NOT NULL,
+
+  FOREIGN KEY (hotelId)
+    REFERENCES hotels (hotelId),
+
+  FOREIGN KEY (userId)
+    REFERENCES users (userId)
 );
-
-CREATE TABLE pools (
-  poolId          INT AUTO_INCREMENT PRIMARY KEY,
-  imageUrl        VARCHAR(255),
-  caption         VARCHAR(255),
-  category        VARCHAR(255),
-  datePosted      VARCHAR(255),
-  helpfulVotes    INT,
-  location        VARCHAR(255),
-  contributions   INT,
-  hotelId         INT,
-  userId          INT
-);
-
-CREATE TABLE gyms (
-  gymId          INT AUTO_INCREMENT PRIMARY KEY,
-  imageUrl        VARCHAR(255),
-  caption         VARCHAR(255),
-  category        VARCHAR(255),
-  datePosted      VARCHAR(255),
-  helpfulVotes    INT,
-  location        VARCHAR(255),
-  contributions   INT,
-  hotelId         INT,
-  userId          INT
-);
-
-CREATE TABLE amenities (
-  amenityId      INT AUTO_INCREMENT PRIMARY KEY,
-  imageUrl        VARCHAR(255),
-  caption         VARCHAR(255),
-  category        VARCHAR(255),
-  datePosted      VARCHAR(255),
-  helpfulVotes    INT,
-  location        VARCHAR(255),
-  contributions   INT,
-  hotelId         INT,
-  userId          INT
-);
-
-CREATE TABLE bathrooms (
-  bathroomId      INT AUTO_INCREMENT PRIMARY KEY,
-  imageUrl        VARCHAR(255),
-  caption         VARCHAR(255),
-  category        VARCHAR(255),
-  datePosted      VARCHAR(255),
-  helpfulVotes    INT,
-  location        VARCHAR(255),
-  contributions   INT,
-  hotelId         INT,
-  userId          INT
-);
-
-CREATE TABLE eventrooms (
-  eventroomId     INT AUTO_INCREMENT PRIMARY KEY,
-  imageUrl        VARCHAR(255),
-  caption         VARCHAR(255),
-  category        VARCHAR(255),
-  datePosted      VARCHAR(255),
-  helpfulVotes    INT,
-  location        VARCHAR(255),
-  contributions   INT,
-  hotelId         INT,
-  userId          INT
-);
-
-CREATE TABLE roomviews (
-  roomviewId      INT AUTO_INCREMENT PRIMARY KEY,
-  imageUrl        VARCHAR(255),
-  caption         VARCHAR(255),
-  category        VARCHAR(255),
-  datePosted      VARCHAR(255),
-  helpfulVotes    INT,
-  location        VARCHAR(255),
-  contributions   INT,
-  hotelId         INT,
-  userId          INT
-);
-
--- Relate photos to hotels
-ALTER TABLE rooms ADD FOREIGN KEY (hotelId) REFERENCES hotels (hotelId);
-ALTER TABLE dinings ADD FOREIGN KEY (hotelId) REFERENCES hotels (hotelId);
-ALTER TABLE pools ADD FOREIGN KEY (hotelId) REFERENCES hotels (hotelId);
-ALTER TABLE gyms ADD FOREIGN KEY (hotelId) REFERENCES hotels (hotelId);
-ALTER TABLE amenities ADD FOREIGN KEY (hotelId) REFERENCES hotels (hotelId);
-ALTER TABLE bathrooms ADD FOREIGN KEY (hotelId) REFERENCES hotels (hotelId);
-ALTER TABLE eventrooms ADD FOREIGN KEY (hotelId) REFERENCES hotels (hotelId);
-ALTER TABLE roomviews ADD FOREIGN KEY (hotelId) REFERENCES hotels (hotelId);
-
--- Relate photos to users
-ALTER TABLE rooms ADD FOREIGN KEY (userId) REFERENCES users (userId);
-ALTER TABLE dinings ADD FOREIGN KEY (userId) REFERENCES users (userId);
-ALTER TABLE pools ADD FOREIGN KEY (userId) REFERENCES users (userId);
-ALTER TABLE gyms ADD FOREIGN KEY (userId) REFERENCES users (userId);
-ALTER TABLE amenities ADD FOREIGN KEY (userId) REFERENCES users (userId);
-ALTER TABLE bathrooms ADD FOREIGN KEY (userId) REFERENCES users (userId);
-ALTER TABLE eventrooms ADD FOREIGN KEY (userId) REFERENCES users (userId);
-ALTER TABLE roomviews ADD FOREIGN KEY (userId) REFERENCES users (userId);
-
