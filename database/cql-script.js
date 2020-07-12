d557051021c59b6fb95d4e452425e88e8fae345f
Nathan Sou <nathanksou@gmail.com>
2020-07-11 22:58:22 -0700

    Complete data generation for cql database

--- database/cql-script.js
+++ database/cql-script.js
@@ -6,26 +6,26 @@ const writePhotos = (start = 1, end = 2, filenumber = 0) => {
   const categories = ['Room & Suite', 'Dining', 'Pool & Beach', 'Gym', 'Hotel & Amenities', 'Bathroom', 'Business Center & Event Rooms', 'View from Room'];
   const captions = ['', faker.lorem.words(), faker.lorem.sentence(), faker.lorem.paragraph()];
   const writer = csvWriter();
-  writer.pipe(fs.createWriteStream(`/Volumes/Elements/photos${filenumber}.csv`));
+  writer.pipe(fs.createWriteStream(`/Volumes/Elements/cphotos${filenumber}.csv`));
   for (let photoId = start; photoId <= end; photoId++) {
     writer.write({
-      photoId: photoId,
-      imageUrl: faker.image.image(),
+      hotelid: faker.random.number(9999999) + 1,
       category: categories[faker.random.number(7)],
-      datePosted: faker.date.recent(90),
+      photoid: photoId,
       caption: captions[faker.random.number(3)],
-      helpfulVotes: faker.random.number(400),
-      hotelId: faker.random.number(9999999) + 1,
-      hotelName: faker.company.companyName(),
-      hotelCity: faker.address.city(),
-      hotelPrice: faker.random.number(1000) + 50,
-      numReviews: faker.random.number(1000),
-      userId: faker.random.number(1999999) + 1
+      contributions: faker.random.number(400),
+      dateposted: faker.date.recent(90),
+      helpfulvotes: faker.random.number(400),
+      hotelcity: faker.address.city(),
+      hotelname: faker.company.companyName(),
+      hotelprice: faker.random.number(1000) + 50,
+      imageurl: faker.image.image(),
+      locationcity: faker.address.city(),
+      locationstate: faker.address.stateAbbr(),
+      numreviews: faker.random.number(1000),
       user: faker.name.findName(),
-      userAvatarUrl: faker.image.avatar(),
-      locationCity: faker.address.city(),
-      locationState: faker.address.stateAbbr(),
-      contributions: faker.random.number(400)
+      useravatarurl: faker.image.avatar(),
+      userid: faker.random.number(1999999) + 1,
     });
     if (photoId % 100000 === 0) {
       console.log(photoId);
@@ -34,3 +34,12 @@ const writePhotos = (start = 1, end = 2, filenumber = 0) => {
   writer.end();
 };
 
+// writePhotos(1, 10, 0);
+// writePhotos(       1,  5000000, 1);
+// writePhotos( 5000001, 10000000, 2);
+// writePhotos(10000001, 15000000, 3);
+// writePhotos(15000001, 20000000, 4);
+// writePhotos(20000001, 25000000, 5);
+// writePhotos(25000001, 30000000, 6);
+// writePhotos(30000001, 35000000, 7);
+// writePhotos(35000001, 40000000, 8);
